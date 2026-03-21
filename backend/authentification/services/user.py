d58d054a6a3f59f519f1af42805bf92dc51b35e1
Jeb129 <elyer129@gmail.com>
2026-03-21 16:33:53 +0300

    Типизация register_user

--- backend/authentification/services/user.py
+++ backend/authentification/services/user.py
@@ -1,6 +1,7 @@
+from authentification.models import CustomUser
 from authentification.serializers import RegisterSerializer
 
-def register_user(data):
+def register_user(data) -> CustomUser:
     serializer = RegisterSerializer(data=data)
     serializer.is_valid(raise_exception=True)
 
