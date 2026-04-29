ee678a17d23e48382d32ed9b34901364ff56cbac
Jeb129 <elyer129@gmail.com>
2026-04-29 18:53:37 +0300

    поправил тесты

--- backend/tests/test_draft_queryset.py
+++ backend/tests/test_draft_queryset.py
@@ -1,57 +1,56 @@
 from datetime import date
-from os import name
 
 import pytest
-from django.contrib.auth.models import User
-from django.db import transaction
 
 from api.models import *
 from api.services.redis.storage import RedisDraftStorage
 from api.services.schedule.draft.context import draft_context
 
+
 def init_scenario():
     semester, _ = Semester.objects.get_or_create(
         name="Тестовый семестр 2026",
-        date_start= date(2026, 2, 1),
-        date_end= date(2026, 7, 31),
+        date_start=date(2026, 2, 1),
+        date_end=date(2026, 7, 31),
     )
-    scenario, _ =  ScheduleScenario.objects.get_or_create(
+    scenario, _ = ScheduleScenario.objects.get_or_create(
         name="Test Scenario",
         semester=semester,
         is_active=True,
-        )
+    )
     return scenario
 
+
 def init_group():
-    inst, _ = Institute.objects.get_or_create(
-        name = "Институт"
-    )
+    inst, _ = Institute.objects.get_or_create(name="Институт")
     programm, _ = StudyProgram.objects.get_or_create(
-        institute = inst,
-        name = "Программа",
-        short_name = "Прог"
+        institute=inst, name="Программа", short_name="Прог"
     )
     return StudyGroup.objects.create(
-        admission_year = 22,
-        stud_program = programm,
-        learning_form = "o",
-        learning_stage = "b",
+        admission_year=22,
+        study_program=programm,
+        learning_form="o",
+        learning_stage="b",
         group_num=1,
         name="Группа",
-        students_count=20
+        students_count=20,
     )
 
+
 def init_lesson(scenario):
-    t1, _ = Timeslot.objects.get_or_create(day=1,week_num=1,order_number=1,time_start="08:30", time_end="10:00")
-    c1, _ = Classroom.objects.get_or_create(num="101",name="аудитория",capacity=30)
+    t1, _ = Timeslot.objects.get_or_create(
+        day=1, week_num=1, order_number=1, time_start="08:30", time_end="10:00"
+    )
+    c1, _ = Classroom.objects.get_or_create(num="101", name="аудитория", capacity=30)
 
     return Lesson.objects.create(
         scenario=scenario,
         timeslot=t1,
         classroom=c1,
         discipline=Discipline.objects.create(name="Дисциплина"),
-        lesson_type=LessonType.objects.create(name="занятие")
+        lesson_type=LessonType.objects.create(name="занятие"),
     )
+
 
 @pytest.mark.django_db
 def test_updated_lesson_fields_are_overridden(redis_client):
@@ -61,7 +60,7 @@ def test_updated_lesson_fields_are_overridden(redis_client):
     scenario = init_scenario()
     lesson = init_lesson(scenario)
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
     storage.update_lesson(lesson.id, {"timeslot": None})
     with draft_context(scenario, storage) as manager:
         draft = Lesson.objects.get(id=lesson.id)
@@ -76,7 +75,7 @@ def test_deleted_lesson_not_visible(redis_client):
     scenario = init_scenario()
     lesson = init_lesson(scenario)
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
     storage.delete_lesson(lesson.id)
 
     with draft_context(scenario, storage):
@@ -91,16 +90,20 @@ def test_created_lessons_are_returned(redis_client):
     """
     scenario = ScheduleScenario.objects.create(name="Test Scenario")
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
-    t1, _ = Timeslot.objects.get_or_create(day=1,week_num=1,order_number=1,time_start="08:30", time_end="10:00")
-    c1, _ = Classroom.objects.get_or_create(num="101",name="аудитория",capacity=30)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
+    t1, _ = Timeslot.objects.get_or_create(
+        day=1, week_num=1, order_number=1, time_start="08:30", time_end="10:00"
+    )
+    c1, _ = Classroom.objects.get_or_create(num="101", name="аудитория", capacity=30)
     storage.clear_created()
-    storage.create_lesson({
-        "timeslot": 1,
-        "classroom": 1,
-        "discipline": 1,
-        "lesson_type": 1,
-    })
+    storage.create_lesson(
+        {
+            "timeslot": 1,
+            "classroom": 1,
+            "discipline": 1,
+            "lesson_type": 1,
+        }
+    )
 
     with draft_context(scenario, storage):
         lessons = list(Lesson.objects.all())
@@ -116,7 +119,7 @@ def test_manager_restored_after_context(redis_client):
     """
     scenario = init_scenario()
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
 
     original_manager = Lesson.objects.__class__
 
@@ -140,12 +143,15 @@ def test_m2m_override(redis_client):
 
     lesson.study_groups.add(group1)
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
     storage.update_lesson(lesson.id, {"study_groups": [group1.id, group2.id]})
 
     with draft_context(scenario, storage):
         draft = Lesson.objects.get(id=lesson.id)
-        assert list(draft.study_groups.values_list("id",flat=True)) == [group1.id, group2.id]
+        assert list(draft.study_groups.values_list("id", flat=True)) == [
+            group1.id,
+            group2.id,
+        ]
         # Проверяем, что реальные связи не менялись
     assert list(lesson.study_groups.all()) == [group1]
 
@@ -158,7 +164,7 @@ def test_outside_context_no_changes(redis_client):
     scenario = init_scenario()
     lesson = init_lesson(scenario)
 
-    storage = RedisDraftStorage(scenario.id, user_id=1,redis=redis_client)
+    storage = RedisDraftStorage(scenario.id, user_id=1, redis=redis_client)
     storage.update_lesson(lesson.id, {"timeslot": 123})  # такого timeslot нет
 
     # ВНЕ контекста — никаких изменений
