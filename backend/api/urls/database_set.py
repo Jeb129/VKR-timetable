e71512c37ce7b6136539c972d143ab80e76bdc26
Jeb129 <elyer129@gmail.com>
2026-06-11 00:46:05 +0300

    Поправил утилсы и ссылки

--- backend/api/urls/database_set.py
+++ backend/api/urls/database_set.py
@@ -1,24 +1,22 @@
 from rest_framework.routers import DefaultRouter
 from api.views import (
     ClassroomViewSet, 
-    BookingViewSet,
     TimeslotViewSet,
     ScheduleScenarioViewSet,
     StudyGroupViewSet,
     TeacherViewSet,
-    BookingTypeViewSet,
     DisciplineViewSet,
-    LessonTypeViewSet
+    LessonTypeViewSet,
+    RequestViewSet
     )
 
 router = DefaultRouter()
 router.register(r'classrooms', ClassroomViewSet, basename='classroom')
-router.register(r'bookings', BookingViewSet, basename='booking')
-router.register(r'booking-types', BookingTypeViewSet, basename='booking-type')
 router.register(r'timeslots', TimeslotViewSet, basename='timeslot')
 router.register(r'scenarios', ScheduleScenarioViewSet, basename='scenario')
 router.register(r'groups', StudyGroupViewSet, basename='group')
 router.register(r'teachers', TeacherViewSet, basename='teacher')
+router.register(r'requests', RequestViewSet, basename='request')
 router.register(r'disciplines', DisciplineViewSet, basename='discipline')
 router.register(r'lesson-types', LessonTypeViewSet, basename='lesson-type')
 
