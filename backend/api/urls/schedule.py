2b810e43bd9646fb77ae598395629fefe23fce4b
Jeb129 <elyer129@gmail.com>
2026-06-12 02:59:20 +0300

    Поправил апи для просмотра расписания

--- backend/api/urls/schedule.py
+++ backend/api/urls/schedule.py
@@ -1,15 +1,11 @@
-from django.urls import path, include 
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from api.views import (TeacherScheduleView, 
                        GroupScheduleView, 
                        ClassroomScheduleView)
-
-router = DefaultRouter()
 
 urlpatterns =[
     path("group/",GroupScheduleView.as_view(), name="group_schedule"),
     path("teacher/", TeacherScheduleView.as_view(), name="teacher_schedule"),
     path("classroom/",ClassroomScheduleView.as_view(), name="classroom_schedule"),
-    path('', include(router.urls)),
 ]
 
