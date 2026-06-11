ad9feab6f52a5638fb9ae79db02a084b4e639b3c
Jeb129 <elyer129@gmail.com>
2026-06-12 01:03:57 +0300

    поправил ссылки для черновиков

--- backend/api/urls/__init__.py
+++ backend/api/urls/__init__.py
@@ -8,7 +8,7 @@ from api.views.statistics import BuildingLoadView
 urlpatterns = [
     path('', include("api.urls.database")),
     path("schedule/",include("api.urls.schedule")),
-    path("scenario/<int:scenario_id>/",include("api.urls.scenario")),
+    path("scenario/<int:scenario_id>/draft/",include("api.urls.draftlessons")),
     path('academic-load/import/', ExcelAPIView.as_view(), name='excel-import-api'),
     path("test/", TestDraftScenarioView.as_view()),
     path('statistics/load/', BuildingLoadView.as_view(), name='stats-load'),
--- backend/api/urls/draftlessons.py
+++ backend/api/urls/draftlessons.py
@@ -7,5 +7,5 @@ router = DefaultRouter()
 router.register(r"lessons",DraftLessonViewSet,basename="lessons")
 
 urlpatterns =[
-    path("draft/",include(router.urls), name="draft"),
+    path("",include(router.urls), name="draft"),
 ]
